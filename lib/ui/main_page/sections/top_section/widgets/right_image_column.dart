dad9a5b482cce12aee648ef8d157a0b330dad920
Dmitriy Makarov <dimamakarov1941@yandex.ru>
2022-07-24 14:37:31 +0300

    add background blobs

--- lib/ui/main_page/sections/top_section/widgets/right_image_column.dart
+++ lib/ui/main_page/sections/top_section/widgets/right_image_column.dart
@@ -11,35 +11,40 @@ class RightImageColumn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(right: 64),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const SizedBox(
-            height: 64,
-          ),
-          Container(
-            color: Colors.red,
-            width: size.width / 2,
-            height: size.height - 64,
-            child: Stack(fit: StackFit.expand, children: [
-              SizedBox(
-                height: size.height / 2,
-                child: SvgPicture.asset(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const SizedBox(
+          height: 64,
+        ),
+        Container(
+          padding: const EdgeInsets.only(left: 128),
+          width: size.width / 2,
+          height: size.height - 64,
+          child: Stack(
+              fit: StackFit.expand,
+              alignment: Alignment.center,
+              children: [
+                Positioned(
+                  height: size.height - 172,
+                  bottom: -size.height / 4,
+                  child: SvgPicture.asset(
                     "assets/icons/abstract_figures/photo_background_blob2.svg",
+                  ),
+                ),
+                Positioned(
+                  height: size.height - 172,
+                  bottom: -size.height / 4 + 20,
+                  child: SvgPicture.asset(
+                    "assets/icons/abstract_figures/photo_background_blob1.svg",
+                  ),
+                ),
+                Image.asset("assets/images/me_photo.png",
                     fit: BoxFit.fitHeight),
-              ),
-              SvgPicture.asset(
-                "assets/icons/abstract_figures/photo_background_blob1.svg",
-                fit: BoxFit.fitHeight,
-              ),
-              Image.asset("assets/images/me_photo.png", fit: BoxFit.fitHeight),
-            ]),
-          )
-        ],
-      ),
+              ]),
+        )
+      ],
     );
   }
 }
